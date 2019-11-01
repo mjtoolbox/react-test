d9bcfaa00638aaa6f380cfe26288a9738c3c5273
mjtoolbox <michael.jo@gmail.com>
2019-11-01 09:08:47 -0700

    Basic router done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import StudentList from './StudentList';
+import Home from './Home';
+import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        {/* <div>
+          <ul>
+            <li>
+              <Link to='/students'>Student List</Link>
+            </li>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+          </ul>
+        </div> */}
+        <Switch>
+          <Route exact path='/hello' component={Home} />
+          <Route path='/hello/students' component={StudentList} />
+          <Route component={NotFound} />
+        </Switch>
+      </Router>
+    );
+  }
+}
+
+function NotFound() {
+  return <h1>Hey not found!</h1>;
 }
 
 export default App;
---
+++ src/StudentList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class StudentList extends React.Component{
+    render() {
+        return <h1>This is where Student List displays.</h1>;
+      }
+}
+
+export default StudentList;
